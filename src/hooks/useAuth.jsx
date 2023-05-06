1e4e2b9921fd0c6416acdd0085c05856998692c5
XBAROX97 <XBAROX97@GMAIL.COM>
2023-05-06 18:32:53 +0200

    hooks

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -2,70 +2,65 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { storage, auth, db, createUserWithEmailAndPassword, updateProfile, setDoc, doc } from "../FireBase";
+import { storage,auth, db, createUserWithEmailAndPassword, updateProfile, setDoc, doc } from "../FireBase";
 
 const useAuth = () => {
     const navigate = useNavigate();
+    
     const [err, setErr] = useState(false);
+
     const [loading, setLoading] = useState(false);
     const [currentUser, setCurrentUser] = useState(null);
 
     const register = async (data, image) => {
-        try {
-          setLoading(true);
-          const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
-      
-          //Create a unique image name
-          const date = new Date().getTime();
-          const displayName = data.displayName; // add this line to define displayName
-          const storageRef = ref(storage, `${displayName + date}`);
-        
-          await uploadBytesResumable(storageRef, image).then(() => {
-            getDownloadURL(storageRef).then(async (downloadURL) => {
-              try {
-                //Update profile
-                await updateProfile(res.user, {
-                  displayName,
-                  photoURL: downloadURL,
-                });
-                //create user on firestore
-                await setDoc(doc(db, "users", res.user.uid), {
-                  uid: res.user.uid,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                displayName: data.displayName,
-                email: data.email, // change email to data.email
-                photoURL: downloadURL,
-                });
-        
-                //create empty user chats on firestore
-                await setDoc(doc(db, "users", res.user.uid), {});
-                navigate("/");
-              } catch (err) {
-                console.log(err);
-                setErr(true);
-                setLoading(false);
-              }
-            });
-          });
-        } catch (err) {
-          setErr(true);
-          setLoading(false);
-        }
-        toast.promise(
-            register(data, image),
-            {
-              loading: "Registering...",
-              success: (newUser) => `Registration successful for ${newUser.firstName + " " + newUser.lastName}!`,
-              error: (error) => `${error}`
-            }
-          );
-      };
-      
-      // move this function call inside the register function
-   
-      
+    
+        setLoading(true)
+        const registrationPromise = createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then(async (userCredential) => {
+                const date = new Date().getTime();
+                const storageRef = ref(storage, `${displayName + date}`);
+          
+                console.log(userCredential, data, image)
+                // Upload the image file to Firebase storage
+                if (image) {
 
+                    
+                  }
+                  
+    
+                await updateProfile(userCredential.user, {
+                    displayName: data.displayName
+                });  
+                const newUser = {
+                    uid: userCredential.user.uid,
+                    email: userCredential.user.email,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    displayName: data.displayName,
+                    image: image
+                };
+                
+                await setDoc(doc(db, 'users', userCredential.user.uid), newUser);
+                navigate("/");
+                setCurrentUser(newUser);
+                setLoading(false)
+                return newUser;
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false)
+                throw new Error(error.code);
+            });
+            toast.promise(
+                registrationPromise,
+                {
+                    loading: "Registering...",
+                    success: (newUser) => `Registration successful for ${newUser.firstName + " " + newUser.lastName}!`,
+                    error: (error) => `${error}`
+                }
+            );
+    };
+    
     const login = async (email, password) => {
         try {
             const userCredential = await auth.signInWithEmailAndPassword(email, password);
