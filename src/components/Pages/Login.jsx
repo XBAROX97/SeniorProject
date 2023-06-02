418015b01bce06839cce652dd3392ff42f5d3b4d
XBAROX97 <XBAROX97@GMAIL.COM>
2023-06-02 15:58:00 +0200

    fdg

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErr(false);
--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -60,7 +60,7 @@ const Register = () => {
   
   return (
 
-    <div className='h-screen relative flex overflow-scroll   '>
+    <div className='h-screen relative flex over   '>
       <div className="absolute inset-0 z-0">
         <img className="w-full max-h-max object-cover" src={backgroundImage} alt="Background" />
         <div className="absolute inset-0 bg-black opacity-60"></div>
